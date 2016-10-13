2f4400fb7099f8ada1e7d507feffac7441a46ccf
German <german.goldenstein@gmail.com>
2016-10-13 14:18:30 -0300

    Adds logs

--- src/app.js
+++ src/app.js
@@ -29,7 +29,7 @@ function processEvent(event) {
         }
 
         console.log("Text", text);
-        console.log("Sender", sender);
+        console.log("Sender", event.sender);
 
         let apiaiRequest = apiAiService.textRequest(text,
             {
